1b6e925667e68109899c9d04700590f31eedf394
root <root@ip-172-26-15-91.us-east-2.compute.internal>
2024-09-05 08:20:31 +0000

    add nignx

--- ssl_issue.sh
+++ ssl_issue.sh
@@ -1,3 +1,5 @@
+# Let's encrypt HTTP\01
+
 curl -sL https://get.acme.sh | sh -s email=aautoops@163.com
 source ~/.bashrc
 ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
@@ -11,4 +13,15 @@ acme.sh --issue -d $dnsname -w /var/www/html/
 acme.sh --install-cert -d $dnsname \
 	--key-file /etc/nginx/ssl/$dnsname.key \
 	--fullchain-file /etc/nginx/ssl/fullchain.cer \
+	--reloadcmd "systemctl reload nginx"
+
+
+
+# Let's encrypt HTTP\01
+export DYNV6_Token="vP1ezxReixiZawqQtRpBZREUdoUCRA"
+acme.sh --issue --dns dns_dynv6 -d *.gcrio.dns.army --dnssleep
+
+acme.sh --install-cert -d *.gcrio.dns.army \
+	--key-file /cert/gcrio.dns.army.key \
+	--fullchain-file /cert/fullchain.pem \
 	--reloadcmd "systemctl reload nginx"
